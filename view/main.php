4269921c8886eae0b18cccafce30eb1ef9b96e04
Roman Yashchuk <ukroficer@mail.ru>
2015-09-13 22:24:48 +0300

    Добавил вид

---
+++ view/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Silence / REPT" />
+
+	<title>Неназванный 2</title>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
