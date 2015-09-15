d11c4c66e6178ad0d1d0505384d710c567c8d3fc
Roman Yashchuk <ukroficer@mail.ru>
2015-09-15 15:50:06 +0300

    autoload

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database','form_validation','session','ion_auth','tpl','firephp','wc_msgs');
 
 /*
 | -------------------------------------------------------------------
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('date','url','text','language','form','html','my','thumb');
 
 /*
 | -------------------------------------------------------------------
