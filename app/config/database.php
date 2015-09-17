27e5314320ec99e802c5831f1b695fbaf4fbfc8e
Roman Yashchuk <ukroficer@mail.ru>
2015-09-17 21:04:27 +0300

    Створення проекту

--- app/config/database.php
+++ app/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'franceUser',
-	'password' => '392854',
-	'database' => 'franceDB',
+	'hostname' => '',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
